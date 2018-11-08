bde6395fc7fe3c95ca2dd4d8442bc1e86e64ea50
Mikhail Meglinksiy <muxmeg@gmail.com>
2018-11-08 14:15:03 +0100

    Initial commit. Created project structure.

---
+++ backend/src/main/java/com/demo/simpleChat/AppConfig.java
@@ -0,0 +1,8 @@
+package com.demo.simpleChat;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+  public static final String REST_SERVICE_PREFIX = "/rest/";
+}
